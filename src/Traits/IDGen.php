47cb803cbd38c8d3672080f349e1021acd22b816
Liu Wei <liuwei73@qq.com>
2020-10-20 17:13:53 -0700

    while $autoGenerateID is true, use IDGenerator to generate ID.

--- src/Traits/IDGen.php
+++ src/Traits/IDGen.php
@@ -7,19 +7,6 @@ trait IDGen
 {
 	public $autoGenerateID = true;
 
-	public static function genID( $day = false, $length = 8 )
-	{
-		if( $day === false )
-			$gen_time = time();
-		else
-			$gen_time = strtotime( $day );
-		$day_prefix = date( "ymd", $gen_time );
-		$id_random = mt_rand( 1, ( pow( 10, $length ) - 1 ) );
-		//随机数前面补0
-		$surfix = str_pad( $id_random, $length, "0", STR_PAD_LEFT );
-		return $day_prefix.$surfix;
-	}
-
 	public static function boot()
 	{
 		parent::boot();
@@ -28,7 +15,8 @@ trait IDGen
 			if( $model->incrementing === false && $model->autoGenerateID === true )
 			{
 				$keyName = $model->getKeyName();
-				$model->$keyName = self::genID();
+				$generator = app( "IDG" );
+				$model->$keyName = $generator->next();
 			}
 		});
 	}
